a71e61c992f430e5d5e617cd091ed6a7d65d3cb8
Hawken Rives <hawkrives@fastmail.fm>
2019-01-02 23:46:01 -0600

    exit the batch script as soon as a story throws an error

--- batch.fish
+++ batch.fish
@@ -3,7 +3,7 @@
 source ./venv/bin/activate.fish
 
 for url in (cat ./urls.txt);
-	python3 scraper/scraper.py $url
+	python3 scraper/scraper.py $url; or exit 1
 end
 
 python3 epuber/epuber.py archive/*
